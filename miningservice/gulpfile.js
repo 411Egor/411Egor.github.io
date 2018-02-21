724cb12bab1304bc11a2cbb518f0e9367a3742e3
Grighorchuk Egor <34961172+411Egor@users.noreply.github.com>
2018-02-21 23:23:49 +0300

    Add files via upload

--- miningservice/gulpfile.js
+++ miningservice/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+    sass = require('gulp-sass');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    fileinclude = require('gulp-file-include'),
+    autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('default', function () {
     return gulp.src('app/css/main.css')
@@ -18,6 +21,29 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./app/css'));
 });
  
-gulp.task('sass:watch', function () {
+gulp.task('libs', function() {
+    return gulp.src([
+        'app/libs/parallax/parallax.min.js',
+        'app/libs/pagescroll/PageScroll2id.min.js',
+        'app/libs/mfp/jquery.magnific-popup.min.js',
+        'app/libs/dist/slideout.min.js',
+        'app/libs/jquery/jquery.min.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+        'app/js/bootstrap.min.js',
+        'app/js/common.js',
+        ])
+        .pipe(concat('min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('watch',['default'], function () {
   gulp.watch('./sass/**/*.sass', ['sass']);
 });
