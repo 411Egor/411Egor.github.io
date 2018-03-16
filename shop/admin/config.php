000d16dcd3082a42f5bd25e329a8f267e503c2ee
411Egor <egor.grighorchuk@gmail.com>
2018-03-17 00:05:21 +0300

    commit

---
+++ shop/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://newopencart.loc/admin/');
+define('HTTP_CATALOG', 'http://newopencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://newopencart.loc/admin/');
+define('HTTPS_CATALOG', 'http://newopencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/newopencart.loc/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/newopencart.loc/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/newopencart.loc/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/newopencart.loc/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/newopencart.loc/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/newopencart.loc/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/newopencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/newopencart.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/newopencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/newopencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/newopencart.loc/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/newopencart.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'opencart.loc');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'newopencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ shop/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://newopencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://newopencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/newopencart.loc/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/newopencart.loc/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/newopencart.loc/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/newopencart.loc/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/newopencart.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/newopencart.loc/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/newopencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/newopencart.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/newopencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/newopencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/newopencart.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'opencart.loc');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'newopencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
